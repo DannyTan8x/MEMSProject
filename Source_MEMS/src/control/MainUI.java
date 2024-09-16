80ace99692621aa45f888808f8beb38fafc06ca6
DannyTan8x <dannytan8xtw@icloud.com>
2024-09-16 14:06:17 +0800

    vscode update

--- Source_MEMS/src/control/MainUI.java
+++ Source_MEMS/src/control/MainUI.java
@@ -1,12 +1,12 @@
 package control;
 
 import java.awt.EventQueue;
-import java.awt.FlowLayout;
+
 import java.awt.Toolkit;
 import java.io.FileNotFoundException;
 
 import javax.swing.JFrame;
-import javax.swing.JInternalFrame;
+
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 
@@ -15,26 +15,12 @@ import control.UIComponents.JMyMenuBar;
 import dao.impl.DbConnection;
 import services.impl.DbSetupServiceImpl;
 import java.awt.BorderLayout;
-import javax.swing.JToolBar;
-import javax.swing.JButton;
-import com.jgoodies.forms.layout.FormLayout;
-import com.jgoodies.forms.layout.ColumnSpec;
-import com.jgoodies.forms.layout.RowSpec;
-import com.jgoodies.forms.layout.FormSpecs;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+
 import javax.swing.JDesktopPane;
-import javax.swing.JLabel;
-import javax.swing.JMenu;
-import javax.swing.JMenuBar;
-import javax.swing.JMenuItem;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
+
 import java.awt.Color;
 import java.awt.Dimension;
 
-import control.Account.LoginIntFrameUI;
-import control.FrameControl;
 
 public class MainUI extends JFrame {
 
